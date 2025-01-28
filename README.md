8bca1c4bfe8c4510eb9b7c4605414cf202d7e91a
Gus Ruben <gusruben@protonmail.com>
2025-01-27 23:59:06 -0500

    Finish magics

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 # magics
 
 Magics is an AI-powered extension of `ls`-- at a glance you can get an idea of the more complex purpose of files and directories.
+
+## Example
+
+```
+ms
+```
+
+```
+total 44
+-rwxrwxrwx  1 gusruben users 23926 Jan 27 23:21 bun.lockb     - Lockfile format for dependency management, likely related to package versions and metadata.
+-rw-r--r--  1 gusruben users  2468 Jan 27 23:56 index.ts      - TypeScript file defining API interactions, filesystem and command execution using ChatGPT service.
+drwxr-xr-x 62 gusruben users  4096 Jan 27 23:21 node_modules  - A list of dependencies for the NodeJS project.
+-rw-r--r--  1 gusruben users   226 Jan 27 23:21 package.json  - Configuration file for Node.js projects, specifying metadata, dependencies, and scripts.
+-rw-r--r--  1 gusruben users   140 Jan 17 02:26 README.md     - Documentation file providing information, instructions, guidelines, and context for a project or repository.
+-rw-r--r--  1 gusruben users   635 Jan 17 02:22 tsconfig.json - TypeScript configuration file specifying compilation options, target version, and other compiler settings.
+```
--- index.ts
+++ index.ts
@@ -1,1 +1,73 @@
-console.log("Hello via Bun!");
+import { exec } from "child_process";
+import { ChatGPTAPI } from "chatgpt";
+import fs from "fs";
+import readline from "readline";
+
+const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY as string
+})
+
+const HEAD_SIZE = 5; // how many lines of context of a file should be passed to chatgpt
+const FILE_PROMPT = "Summarize the purpose of `{}` as concisely as possible, in no more than 15 words. Do not give any explanation, just a short description of the file contents and usagel, for example a file called `main.py` with complex Python code should just be summarized as 'Main Python file'. The file in this case is called `{}`; here are the first 10 lines of the contents:\n{}"
+
+
+async function prompt(text: string) {
+    const res = await api.sendMessage(text);
+    return res.text;
+}
+// https://stackoverflow.com/a/72265631
+async function fileHead(path: string) {
+    let output = "";
+    
+    const inputStream = fs.createReadStream(path);
+    let i = 0;
+    try {
+        for await (const line of readline.createInterface(inputStream)) {
+            output += line;
+            if (i++ <= HEAD_SIZE) {
+                output += line + '\n';
+            } else {
+                return output;
+            }
+        }
+        return ''; // If the file is empty.
+    }
+    finally {
+        inputStream.destroy(); // Destroy file stream.
+    }
+}
+
+
+if (process.argv[0] == "/usr/bin/bun") process.argv.shift();
+
+
+function runCommand(command: string): Promise<string> {
+    return new Promise(resolve => {
+        exec(command, (_, stdout) => resolve(stdout));
+    })
+}
+
+(async () => {
+    const ls = (await runCommand("ls -l " + process.argv.slice(1).join(" "))).split("\n");
+    let directory = process.argv.at(-1);
+    if (!directory?.endsWith("/")) directory += "/"
+    
+    // "header" - just "total " + the number of files/folders
+    console.log(ls.shift());
+
+    const maxLineLength = Math.max.apply(null, ls.map(line => line.length));
+
+    // for each file
+    for (let line of ls) {
+        const filename = line.split(/ +/).slice(8).join(" ");
+        if (!filename) continue;
+
+        const fileStats = fs.statSync(directory + filename);
+        let summary;
+        if (fileStats.isFile()) {
+            summary = await prompt(FILE_PROMPT.replace("{}", filename).replace("{}", filename).replace("{}", await fileHead(directory + filename)));
+        }
+
+        console.log(line + " ".repeat(maxLineLength - line.length), "-", summary)
+    }
+})();
